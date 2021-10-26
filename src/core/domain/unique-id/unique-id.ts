2cb57934df2da65efb52def0284f49c2597c1c52
JM Orbegoso <dev@jmorbegoso.com>
2021-10-26 14:01:47 -0500

    Create UniqueId class

---
+++ src/core/domain/unique-id/unique-id.ts
@@ -0,0 +1,16 @@
+import { ValueObject } from '../value-objects';
+import { v4 as createUuid, validate as isUuidValid } from 'uuid';
+
+export class UniqueId extends ValueObject<string> {
+	validate(value: string): void {
+		if (!value) throw new Error('Invalid id.');
+
+		if (!isUuidValid(value)) throw new Error('Invalid id.');
+	}
+
+	static create(value?: string): UniqueId {
+		if (!value) return new UniqueId(createUuid());
+
+		return new UniqueId(value);
+	}
+}
