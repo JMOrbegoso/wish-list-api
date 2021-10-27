59a5375d24893f2d8979741d862b01598aa819ec
JM Orbegoso <dev@jmorbegoso.com>
2021-10-26 19:35:23 -0500

    Create AggregateRoot class

---
+++ src/core/domain/aggregate-roots/aggregate-root.ts
@@ -0,0 +1,18 @@
+import { Entity } from '../entities';
+import { DomainEvent } from '../domain-events';
+
+export abstract class AggregateRoot extends Entity {
+	private readonly _domainEvents: DomainEvent[] = [];
+
+	get domainEvents(): DomainEvent[] {
+		return Object.assign([], this._domainEvents);
+	}
+
+	public addDomainEvent(domainEvent: DomainEvent): void {
+		this._domainEvents.push(domainEvent);
+	}
+
+	public clearDomainEvents(): void {
+		this._domainEvents.splice(0, this._domainEvents.length);
+	}
+}
