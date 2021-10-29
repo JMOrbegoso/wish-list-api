90ec5a0753157559ef0d4754f43753aaa5aee9ce
JM Orbegoso <dev@jmorbegoso.com>
2021-10-29 09:40:34 -0500

    Create Repository interface

---
+++ src/core/domain/repositories/repository.ts
@@ -0,0 +1,12 @@
+import { AggregateRoot } from '../aggregate-roots';
+import { UniqueId } from '../unique-id';
+
+export interface Repository<T extends AggregateRoot> {
+	getAll(): Promise<T[]>;
+
+	find(id: UniqueId): Promise<T>;
+
+	add(entity: T): Promise<void>;
+
+	delete(id: UniqueId): Promise<void>;
+}
