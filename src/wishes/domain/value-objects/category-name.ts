deb6bd67bd9b0eb6de46abf2edffcad91131b7b7
JM Orbegoso <dev@jmorbegoso.com>
2021-10-31 07:18:25 -0500

    Create CategoryName value object

---
+++ src/wishes/domain/value-objects/category-name.ts
@@ -0,0 +1,11 @@
+import { ValueObject } from '../../../core/domain/value-objects';
+
+export class CategoryName extends ValueObject<string> {
+  validate(value: string): void {
+    if (!value) throw new Error('Invalid category name.');
+  }
+
+  static create(value: string): CategoryName {
+    return new CategoryName(value);
+  }
+}
