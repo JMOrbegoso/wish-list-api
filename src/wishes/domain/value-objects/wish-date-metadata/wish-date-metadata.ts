c8667aadb0efef21c344199e59127f66e9fea555
JM Orbegoso <dev@jmorbegoso.com>
2021-11-01 20:39:06 -0500

    Create WishDateMetadata value object

---
+++ src/wishes/domain/value-objects/wish-date-metadata/wish-date-metadata.ts
@@ -0,0 +1,16 @@
+import { ValueObject } from '../../../../core/domain/value-objects';
+
+export class WishDateMetadata extends ValueObject<number> {
+  validate(value: number): void {
+    if (!value) throw new Error('Invalid date.');
+  }
+
+  static create(value?: number): WishDateMetadata {
+    if (!value) {
+      const milliseconds = Date.now();
+      return new WishDateMetadata(milliseconds);
+    }
+
+    return new WishDateMetadata(value);
+  }
+}
