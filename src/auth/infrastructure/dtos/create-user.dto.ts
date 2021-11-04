a86cb15d705af49cad8439618bfd552b6134ca3b
JM Orbegoso <dev@jmorbegoso.com>
2021-11-04 18:29:17 -0500

    Create create-user.dto.ts

---
+++ src/auth/infrastructure/dtos/create-user.dto.ts
@@ -0,0 +1,10 @@
+export class CreateUserDto {
+  email: string;
+  userName: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  birthday: number;
+  biography: string;
+  profilePicture: string;
+}
