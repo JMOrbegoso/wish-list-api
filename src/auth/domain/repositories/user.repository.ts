3a791831aed6c5fce7823660152542065b305dfd
JM Orbegoso <dev@jmorbegoso.com>
2021-11-05 11:19:24 -0500

    Update UserRepository adding the function getOneByUserName

--- src/auth/domain/repositories/user.repository.ts
+++ src/auth/domain/repositories/user.repository.ts
@@ -1,7 +1,7 @@
 import { Repository } from '../../../core/domain/repositories';
 import { User } from '../entities';
-import { UniqueId } from '../../../core/domain/value-objects';
+import { UserName } from '../value-objects';
 
 export interface UserRepository extends Repository<User> {
-  verifyUser(userId: UniqueId): Promise<void>;
+  getOneByUserName(userName: UserName): Promise<User>;
 }
--- src/auth/infrastructure/persistence/repositories/user.mongodb.repository.ts
+++ src/auth/infrastructure/persistence/repositories/user.mongodb.repository.ts
@@ -5,14 +5,24 @@ import {
 import { UserRepository } from '../../../domain/repositories';
 import { User } from '../../../domain/entities';
 import { UserEntity } from '../entities';
-import { UniqueId } from '../../../../core/domain/value-objects';
 import { toUser, toUserEntity } from '../../mappings';
+import { UniqueId } from '../../../../core/domain/value-objects';
+import { UserName } from '../../../../auth/domain/value-objects';
 
 @MikroOrmRepository(UserEntity)
 export class UserMongoDbRepository
   extends EntityRepository<UserEntity>
   implements UserRepository
 {
+  async getOneByUserName(userName: UserName): Promise<User> {
+    const userEntity = await this.findOne({
+      userName: userName.getUserName,
+    });
+    if (!userEntity) return null;
+    const user = toUser(userEntity);
+    return user;
+  }
+
   async getAll(): Promise<User[]> {
     const usersEntities = await this.findAll();
     const users = usersEntities.map((u) => toUser(u));
