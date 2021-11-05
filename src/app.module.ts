02773387dd279c025b9e0fe880109d1847f02f27
JM Orbegoso <dev@jmorbegoso.com>
2021-11-05 15:26:46 -0500

    Update AppModule adding UsersModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/infrastructure/presentation/users.module';
 
 @Module({
-  imports: [MikroOrmModule.forRoot()],
+  imports: [MikroOrmModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
