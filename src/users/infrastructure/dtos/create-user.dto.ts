bf8dc52600317122affe073c3975ed23c9a0d43e
JM Orbegoso <dev@jmorbegoso.com>
2021-11-05 19:03:18 -0500

    Update CreateUserDto adding info for Swagger

--- src/users/infrastructure/dtos/create-user.dto.ts
+++ src/users/infrastructure/dtos/create-user.dto.ts
@@ -1,10 +1,67 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'Email of the new user.',
+    example: 'john@doe.com',
+  })
   email: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'UserName of the new user.',
+    example: 'john_doe',
+  })
   userName: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'Password of the new user.',
+    example: 'password',
+  })
   password: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'First name of the new user.',
+    example: 'John',
+  })
   firstName: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'Last name of the new user.',
+    example: 'Doe',
+  })
   lastName: string;
+
+  @ApiProperty({
+    type: Number,
+    required: true,
+    description: 'Birthday in milliseconds of the new user.',
+    example: 1636128526164,
+  })
   birthday: number;
+
+  @ApiProperty({
+    type: String,
+    required: false,
+    description: 'Biography of the new user.',
+    example: 'A nice person.',
+  })
   biography: string;
+
+  @ApiProperty({
+    type: String,
+    required: false,
+    description: 'Profile picture url of the new user.',
+    example: 'https://www.example.com',
+  })
   profilePicture: string;
 }
