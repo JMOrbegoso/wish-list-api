803bbb2c685bb553a63dde2f17c0a03ec8c7401f
JM Orbegoso <dev@jmorbegoso.com>
2021-11-06 20:30:40 -0500

    Create DeleteUser command

--- src/users/application/commands/index.ts
+++ src/users/application/commands/index.ts
@@ -1,2 +1,5 @@
 export * from './create-user/create-user.command';
 export * from './create-user/create-user.handler';
+
+export * from './delete-user/delete-user.command';
+export * from './delete-user/delete-user.handler';
