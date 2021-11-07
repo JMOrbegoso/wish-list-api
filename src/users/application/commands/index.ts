bfe6a04a7d19d84c5f22e970e0c292151c553b20
JM Orbegoso <dev@jmorbegoso.com>
2021-11-06 20:42:44 -0500

    Create UndeleteUser command

--- src/users/application/commands/index.ts
+++ src/users/application/commands/index.ts
@@ -3,3 +3,6 @@ export * from './create-user/create-user.handler';
 
 export * from './delete-user/delete-user.command';
 export * from './delete-user/delete-user.handler';
+
+export * from './undelete-user/undelete-user.command';
+export * from './undelete-user/undelete-user.handler';
---
+++ src/users/application/commands/undelete-user/undelete-user.command.ts
@@ -0,0 +1,3 @@
+export class UndeleteUserCommand {
+  constructor(public readonly id: string) {}
+}
