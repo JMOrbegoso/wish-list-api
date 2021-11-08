fe0e7222164b9875faa765dbf8ca871c2ad883b9
JM Orbegoso <dev@jmorbegoso.com>
2021-11-07 20:33:06 -0500

    Create AccountsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/infrastructure/presentation/users.module';
+import { AccountsModule } from './users/infrastructure/presentation/accounts.module';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), UsersModule],
+  imports: [MikroOrmModule.forRoot(), AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/infrastructure/presentation/accounts.module.ts
@@ -0,0 +1,65 @@
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { UserRepositoryMongoDb } from '../persistence/repositories';
+import { UnitOfWork } from '../../../core/domain/repositories';
+import { UnitOfWorkMongoDb } from '../../../core/infrastructure/repositories';
+import { UserEntity } from '../persistence/entities';
+import {
+  EncryptionService,
+  UniqueIdGeneratorService,
+} from '../../../users/application/services';
+import {
+  EncryptionServiceBcrypt,
+  UniqueIdGeneratorServiceMongoDb,
+} from '../services';
+import { AccountsController } from './accounts.controller';
+import {
+  GetUsersHandler,
+  GetUserByIdHandler,
+  GetUserByEmailHandler,
+  GetUserByUserNameHandler,
+} from '../../../users/application/queries';
+import {
+  BlockUserHandler,
+  CreateUserHandler,
+  UpdateUserHandler,
+  DeleteUserHandler,
+  UnblockUserHandler,
+  UndeleteUserHandler,
+} from '../../application/commands';
+
+const queryHandlers = [
+  GetUsersHandler,
+  GetUserByIdHandler,
+  GetUserByEmailHandler,
+  GetUserByUserNameHandler,
+];
+const commandHandlers = [
+  CreateUserHandler,
+  UpdateUserHandler,
+  DeleteUserHandler,
+  UndeleteUserHandler,
+  BlockUserHandler,
+  UnblockUserHandler,
+];
+
+@Module({
+  controllers: [AccountsController],
+  imports: [
+    MikroOrmModule.forFeature([UserEntity]),
+    UserRepositoryMongoDb,
+    CqrsModule,
+  ],
+  providers: [
+    { provide: UnitOfWork, useClass: UnitOfWorkMongoDb },
+    { provide: EncryptionService, useClass: EncryptionServiceBcrypt },
+    {
+      provide: UniqueIdGeneratorService,
+      useClass: UniqueIdGeneratorServiceMongoDb,
+    },
+    ...queryHandlers,
+    ...commandHandlers,
+  ],
+})
+export class AccountsModule {}
