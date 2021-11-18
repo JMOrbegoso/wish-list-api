c6107730bc6810856e643cc41073bb755cc47e73
JM Orbegoso <dev@jmorbegoso.com>
2021-11-18 13:29:34 -0500

    Update AccountsController adding descriptions with Swagger

--- src/users/infrastructure/presentation/accounts.controller.ts
+++ src/users/infrastructure/presentation/accounts.controller.ts
@@ -46,34 +46,34 @@ import {
 export class AccountsController {
   constructor(private commandBus: CommandBus, private queryBus: QueryBus) {}
 
-  @ApiOkResponse({ type: [OutputUserDto] })
+  @ApiOkResponse({ type: [OutputUserDto], description: 'Users found.' })
   @Get()
   async getAllUsers(): Promise<OutputUserDto[]> {
     const query = new GetUsersQuery();
     return await this.queryBus.execute(query);
   }
 
-  @ApiOkResponse({ type: OutputUserDto })
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ type: OutputUserDto, description: 'User found.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Get(':id')
   async getUserById(@Param() params: UserIdDto): Promise<OutputUserDto> {
     const query = new GetUserByIdQuery(params.id);
     return await this.queryBus.execute(query);
   }
 
-  @ApiOkResponse({ type: OutputUserDto })
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ type: OutputUserDto, description: 'User found.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Get('email/:email')
   async getUserByEmail(@Param() params: UserEmailDto): Promise<OutputUserDto> {
     const query = new GetUserByEmailQuery(params.email);
     return await this.queryBus.execute(query);
   }
 
-  @ApiOkResponse({ type: OutputUserDto })
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ type: OutputUserDto, description: 'User found.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Get('username/:userName')
   async getUserByUserName(
     @Param() params: UserNameDto,
@@ -84,7 +84,7 @@ export class AccountsController {
 
   @ApiBody({ required: true, type: CreateUserDto })
   @ApiCreatedResponse({ description: 'User created successfully.' })
-  @ApiBadRequestResponse()
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Post()
   async register(@Body() dto: CreateUserDto): Promise<void> {
     const command: CreateUserCommand = Mapper.toCreateUserCommand(dto);
@@ -92,9 +92,9 @@ export class AccountsController {
   }
 
   @ApiBody({ required: true, type: UpdateUserDto })
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ description: 'User updated successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Patch(':id')
   async update(
     @Param() params: UserIdDto,
@@ -106,36 +106,36 @@ export class AccountsController {
     await this.commandBus.execute(command);
   }
 
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ description: 'User blocked successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Patch('block/:id')
   async blockUser(@Param() params: UserIdDto): Promise<void> {
     const command = new BlockUserCommand(params.id);
     await this.commandBus.execute(command);
   }
 
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ description: 'User unblocked successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Patch('unblock/:id')
   async unblockUser(@Param() params: UserIdDto): Promise<void> {
     const command = new UnblockUserCommand(params.id);
     await this.commandBus.execute(command);
   }
 
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ description: 'User deleted successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Delete(':id')
   async deleteUser(@Param() params: UserIdDto): Promise<void> {
     const command = new DeleteUserCommand(params.id);
     await this.commandBus.execute(command);
   }
 
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  @ApiBadRequestResponse()
+  @ApiOkResponse({ description: 'User undeleted successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Patch('undelete/:id')
   async undeleteUser(@Param() params: UserIdDto): Promise<void> {
     const command = new UndeleteUserCommand(params.id);
