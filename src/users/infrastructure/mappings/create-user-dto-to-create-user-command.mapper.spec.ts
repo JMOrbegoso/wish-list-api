8217ec87bca9c14ef6c4b4373fc16ceb30b35a20
JM Orbegoso <dev@jmorbegoso.com>
2021-11-18 18:48:55 -0500

    Update create-user-dto-to-create-user-command.mapper.spec.ts

--- src/users/infrastructure/mappings/create-user-dto-to-create-user-command.mapper.spec.ts
+++ src/users/infrastructure/mappings/create-user-dto-to-create-user-command.mapper.spec.ts
@@ -1,47 +1,135 @@
+import { mocked } from 'ts-jest/utils';
 import { createUserDtoToCreateUserCommand } from '.';
 import { CreateUserDto } from '../dtos';
+
+const validValues = [
+  [
+    mocked<CreateUserDto>({
+      id: 'id-0',
+      email: 'email0@email.com',
+      userName: 'UserName0',
+      password: 'password0',
+      firstName: 'FirstName0',
+      lastName: 'LastName0',
+      birthday: 1,
+      biography: 'A nice person 0.',
+      profilePicture: 'https://www.example.com/0.jpg',
+    } as unknown as CreateUserDto),
+  ],
+  [
+    mocked<CreateUserDto>({
+      id: 'id-1',
+      email: 'email1@email.com',
+      userName: 'UserName1',
+      password: 'hash1',
+      firstName: 'FirstName1',
+      lastName: 'LastName1',
+      birthday: 1,
+      biography: null,
+      profilePicture: 'https://www.example.com/1.jpg',
+    } as unknown as CreateUserDto),
+  ],
+  [
+    mocked<CreateUserDto>({
+      id: 'id-2',
+      email: 'email2@email.com',
+      userName: 'UserName2',
+      password: 'hash2',
+      firstName: 'FirstName2',
+      lastName: 'LastName2',
+      birthday: 1,
+      biography: 'A nice person 2.',
+      profilePicture: null,
+    } as unknown as CreateUserDto),
+  ],
+  [
+    mocked<CreateUserDto>({
+      id: 'id-3',
+      userName: 'email3@email.com',
+      email: 'UserName3',
+      password: 'hash3',
+      firstName: 'FirstName3',
+      lastName: 'LastName3',
+      birthday: 1,
+      biography: 'A nice person 3.',
+    } as unknown as CreateUserDto),
+  ],
+  [
+    mocked<CreateUserDto>({
+      id: 'id-4',
+      email: 'email4@email.com',
+      userName: 'UserName4',
+      password: 'hash4',
+      firstName: 'FirstName4',
+      lastName: 'LastName4',
+      birthday: 1,
+      biography: null,
+      profilePicture: null,
+    } as unknown as CreateUserDto),
+  ],
+  [
+    mocked<CreateUserDto>({
+      id: 'id-5',
+      email: 'email5@email.com',
+      userName: 'UserName5',
+      password: 'hash5',
+      firstName: 'FirstName5',
+      lastName: 'LastName5',
+      birthday: 1,
+      biography: null,
+      profilePicture: 'https://www.example.com/5.jpg',
+    } as unknown as CreateUserDto),
+  ],
+  [
+    mocked<CreateUserDto>({
+      id: 'id-6',
+      email: 'email6@email.com',
+      userName: 'UserName6',
+      password: 'hash6',
+      firstName: 'FirstName6',
+      lastName: 'LastName6',
+      birthday: 1,
+      biography: 'A nice person 6.',
+      profilePicture: null,
+    } as unknown as CreateUserDto),
+  ],
+  [
+    mocked<CreateUserDto>({
+      id: 'id-7',
+      email: 'email7@email.com',
+      userName: 'UserName7',
+      password: 'hash7',
+      firstName: 'FirstName7',
+      lastName: 'LastName7',
+      birthday: 1,
+    } as unknown as CreateUserDto),
+  ],
+];
 
 describe('users', () => {
   describe('infrastructure', () => {
     describe('mappings', () => {
       describe('CreateUserDto to CreateUserCommand', () => {
-        const id = 'id';
-        const email = 'john@DOE.com';
-        const username = 'John_Doe';
-        const password = 'password';
-        const firstNameText = 'John';
-        const lastNameText = 'Doe';
-        const birthDate = new Date('2000-05-05');
-        const birthDateMilliseconds = birthDate.getTime();
-        const bio = 'A nice person.';
+        test.each(validValues)(
+          'should map CreateUserDto to CreateUserCommand keeping all the property values',
+          (dto: CreateUserDto) => {
+            // Arrange
 
-        it('should map CreateUserDto to a CreateUserCommand keeping all the property values', () => {
-          // Arrange
-          const dto = new CreateUserDto();
-          dto.id = id;
-          dto.email = email;
-          dto.userName = username;
-          dto.password = password;
-          dto.firstName = firstNameText;
-          dto.lastName = lastNameText;
-          dto.birthday = birthDateMilliseconds;
-          dto.biography = bio;
-          dto.profilePicture = null;
+            // Act
+            const command = createUserDtoToCreateUserCommand(dto);
 
-          // Act
-          const command = createUserDtoToCreateUserCommand(dto);
-
-          // Assert
-          expect(command.id).toBe(id);
-          expect(command.email).toBe(email);
-          expect(command.userName).toBe(username);
-          expect(command.password).toBe(password);
-          expect(command.firstName).toBe(firstNameText);
-          expect(command.lastName).toBe(lastNameText);
-          expect(command.birthday).toBe(birthDateMilliseconds);
-          expect(command.biography).toBe(bio);
-          expect(command.profilePicture).toBeNull();
-        });
+            // Assert
+            expect(command.id).toBe(dto.id);
+            expect(command.email).toBe(dto.email);
+            expect(command.userName).toBe(dto.userName);
+            expect(command.password).toBe(dto.password);
+            expect(command.firstName).toBe(dto.firstName);
+            expect(command.lastName).toBe(dto.lastName);
+            expect(command.birthday).toBe(dto.birthday);
+            expect(command.biography).toBe(dto.biography);
+            expect(command.profilePicture).toBe(dto.profilePicture);
+          },
+        );
       });
     });
   });
