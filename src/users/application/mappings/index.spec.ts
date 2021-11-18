7bd0271899f3e3c3ef3cd37ef422b2e3d4cae01b
JM Orbegoso <dev@jmorbegoso.com>
2021-11-18 15:55:20 -0500

    Update tests of Application Layer mappings adding more valid values

--- src/users/application/mappings/index.spec.ts
+++ src/users/application/mappings/index.spec.ts
@@ -17,16 +17,36 @@ const validValues = [
       passwordHash: {
         getPasswordHash: 'hash0',
       },
-      isVerified: { getStatus: true },
-      isBlocked: { getStatus: false },
-      firstName: { getFirstName: 'FirstName0' },
-      lastName: { getLastName: 'LastName0' },
-      birthday: { getMilliseconds: 1 },
-      createdAt: { getMilliseconds: 2 },
-      updatedAt: { getMilliseconds: 3 },
-      biography: { getBiography: 'A nice person 0.' },
-      profilePicture: { getUrl: 'https://www.example.com/0.jpg' },
-      deletedAt: { getMilliseconds: 4 },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName0',
+      },
+      lastName: {
+        getLastName: 'LastName0',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
+      biography: {
+        getBiography: 'A nice person 0.',
+      },
+      profilePicture: {
+        getUrl: 'https://www.example.com/0.jpg',
+      },
+      deletedAt: {
+        getMilliseconds: 4,
+      },
     } as unknown as User),
   ],
   [
@@ -43,16 +63,34 @@ const validValues = [
       passwordHash: {
         getPasswordHash: 'hash1',
       },
-      isVerified: { getStatus: true },
-      isBlocked: { getStatus: false },
-      firstName: { getFirstName: 'FirstName1' },
-      lastName: { getLastName: 'LastName1' },
-      birthday: { getMilliseconds: 10 },
-      createdAt: { getMilliseconds: 20 },
-      updatedAt: { getMilliseconds: 30 },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName1',
+      },
+      lastName: {
+        getLastName: 'LastName1',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
       biography: null,
-      profilePicture: { getUrl: 'https://www.example.com/1.jpg' },
-      deletedAt: { getMilliseconds: 4 },
+      profilePicture: {
+        getUrl: 'https://www.example.com/1.jpg',
+      },
+      deletedAt: {
+        getMilliseconds: 4,
+      },
     } as unknown as User),
   ],
   [
@@ -69,16 +107,34 @@ const validValues = [
       passwordHash: {
         getPasswordHash: 'hash2',
       },
-      isVerified: { getStatus: true },
-      isBlocked: { getStatus: false },
-      firstName: { getFirstName: 'FirstName2' },
-      lastName: { getLastName: 'LastName2' },
-      birthday: { getMilliseconds: 11 },
-      createdAt: { getMilliseconds: 22 },
-      updatedAt: { getMilliseconds: 33 },
-      biography: { getBiography: 'A nice person 2.' },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName2',
+      },
+      lastName: {
+        getLastName: 'LastName2',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
+      biography: {
+        getBiography: 'A nice person 2.',
+      },
       profilePicture: null,
-      deletedAt: { getMilliseconds: 4 },
+      deletedAt: {
+        getMilliseconds: 4,
+      },
     } as unknown as User),
   ],
   [
@@ -86,24 +142,42 @@ const validValues = [
       id: {
         getId: 'id-3',
       },
-      email: {
+      userName: {
         getEmail: 'email3@email.com',
       },
-      userName: {
+      email: {
         getUserName: 'UserName3',
       },
       passwordHash: {
         getPasswordHash: 'hash3',
       },
-      isVerified: { getStatus: true },
-      isBlocked: { getStatus: false },
-      firstName: { getFirstName: 'FirstName3' },
-      lastName: { getLastName: 'LastName3' },
-      birthday: { getMilliseconds: 111 },
-      createdAt: { getMilliseconds: 222 },
-      updatedAt: { getMilliseconds: 333 },
-      biography: { getBiography: 'A nice person 3.' },
-      profilePicture: { getUrl: 'https://www.example.com/3.jpg' },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName3',
+      },
+      lastName: {
+        getLastName: 'LastName3',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
+      biography: {
+        getBiography: 'A nice person 3.',
+      },
+      profilePicture: {
+        getUrl: 'https://www.example.com/3.jpg',
+      },
       deletedAt: null,
     } as unknown as User),
   ],
@@ -121,13 +195,153 @@ const validValues = [
       passwordHash: {
         getPasswordHash: 'hash4',
       },
-      isVerified: { getStatus: true },
-      isBlocked: { getStatus: false },
-      firstName: { getFirstName: 'FirstName4' },
-      lastName: { getLastName: 'LastName4' },
-      birthday: { getMilliseconds: 1111 },
-      createdAt: { getMilliseconds: 2222 },
-      updatedAt: { getMilliseconds: 3333 },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName4',
+      },
+      lastName: {
+        getLastName: 'LastName4',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
+      biography: null,
+      profilePicture: null,
+      deletedAt: {
+        getMilliseconds: 4,
+      },
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-5',
+      },
+      email: {
+        getEmail: 'email5@email.com',
+      },
+      userName: {
+        getUserName: 'UserName5',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash5',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName5',
+      },
+      lastName: {
+        getLastName: 'LastName5',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
+      biography: null,
+      profilePicture: {
+        getUrl: 'https://www.example.com/5.jpg',
+      },
+      deletedAt: null,
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-6',
+      },
+      email: {
+        getEmail: 'email6@email.com',
+      },
+      userName: {
+        getUserName: 'UserName6',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash6',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName6',
+      },
+      lastName: {
+        getLastName: 'LastName6',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
+      biography: {
+        getBiography: 'A nice person 6.',
+      },
+      profilePicture: null,
+      deletedAt: null,
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-7',
+      },
+      email: {
+        getEmail: 'email7@email.com',
+      },
+      userName: {
+        getUserName: 'UserName7',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash7',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName7',
+      },
+      lastName: {
+        getLastName: 'LastName7',
+      },
+      birthday: {
+        getMilliseconds: 1,
+      },
+      createdAt: {
+        getMilliseconds: 2,
+      },
+      updatedAt: {
+        getMilliseconds: 3,
+      },
       biography: null,
       profilePicture: null,
       deletedAt: null,
