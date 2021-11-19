b12944cdd1666fc6a86eb55a5eb95138e8c24a76
JM Orbegoso <dev@jmorbegoso.com>
2021-11-18 19:37:08 -0500

    Update user-to-user-entity.mapper.spec.ts

--- src/users/infrastructure/mappings/user-to-user-entity.mapper.spec.ts
+++ src/users/infrastructure/mappings/user-to-user-entity.mapper.spec.ts
@@ -1,84 +1,399 @@
+import { mocked } from 'ts-jest/utils';
 import { userToUserEntity } from '.';
 import { User } from '../../domain/entities';
-import { UniqueId, MillisecondsDate } from '../../../core/domain/value-objects';
-import {
-  Biography,
-  Email,
-  FirstName,
-  IsVerified,
-  IsBlocked,
-  LastName,
-  PasswordHash,
-  UserName,
-} from '../../domain/value-objects';
-import { normalizeString } from '../../../core/helpers';
+
+const validValues = [
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-0',
+      },
+      email: {
+        getEmail: 'email0@email.com',
+      },
+      userName: {
+        getUserName: 'UserName0',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash0',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName0',
+      },
+      lastName: {
+        getLastName: 'LastName0',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: {
+        getBiography: 'A nice person 0.',
+      },
+      profilePicture: {
+        getUrl: 'https://www.example.com/0.jpg',
+      },
+      deletedAt: {
+        getDate: new Date(2001, 5, 5),
+      },
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-1',
+      },
+      email: {
+        getEmail: 'email1@email.com',
+      },
+      userName: {
+        getUserName: 'UserName1',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash1',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName1',
+      },
+      lastName: {
+        getLastName: 'LastName1',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: null,
+      profilePicture: {
+        getUrl: 'https://www.example.com/1.jpg',
+      },
+      deletedAt: {
+        getDate: new Date(2001, 5, 5),
+      },
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-2',
+      },
+      email: {
+        getEmail: 'email2@email.com',
+      },
+      userName: {
+        getUserName: 'UserName2',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash2',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName2',
+      },
+      lastName: {
+        getLastName: 'LastName2',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: {
+        getBiography: 'A nice person 2.',
+      },
+      profilePicture: null,
+      deletedAt: {
+        getDate: new Date(2001, 5, 5),
+      },
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-3',
+      },
+      userName: {
+        getEmail: 'email3@email.com',
+      },
+      email: {
+        getUserName: 'UserName3',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash3',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName3',
+      },
+      lastName: {
+        getLastName: 'LastName3',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: {
+        getBiography: 'A nice person 3.',
+      },
+      profilePicture: {
+        getUrl: 'https://www.example.com/3.jpg',
+      },
+      deletedAt: null,
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-4',
+      },
+      email: {
+        getEmail: 'email4@email.com',
+      },
+      userName: {
+        getUserName: 'UserName4',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash4',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName4',
+      },
+      lastName: {
+        getLastName: 'LastName4',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: null,
+      profilePicture: null,
+      deletedAt: {
+        getDate: new Date(2001, 5, 5),
+      },
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-5',
+      },
+      email: {
+        getEmail: 'email5@email.com',
+      },
+      userName: {
+        getUserName: 'UserName5',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash5',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName5',
+      },
+      lastName: {
+        getLastName: 'LastName5',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: null,
+      profilePicture: {
+        getUrl: 'https://www.example.com/5.jpg',
+      },
+      deletedAt: null,
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-6',
+      },
+      email: {
+        getEmail: 'email6@email.com',
+      },
+      userName: {
+        getUserName: 'UserName6',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash6',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName6',
+      },
+      lastName: {
+        getLastName: 'LastName6',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: {
+        getBiography: 'A nice person 6.',
+      },
+      profilePicture: null,
+      deletedAt: null,
+    } as unknown as User),
+  ],
+  [
+    mocked<User>({
+      id: {
+        getId: 'id-7',
+      },
+      email: {
+        getEmail: 'email7@email.com',
+      },
+      userName: {
+        getUserName: 'UserName7',
+      },
+      passwordHash: {
+        getPasswordHash: 'hash7',
+      },
+      isVerified: {
+        getStatus: true,
+      },
+      isBlocked: {
+        getStatus: false,
+      },
+      firstName: {
+        getFirstName: 'FirstName7',
+      },
+      lastName: {
+        getLastName: 'LastName7',
+      },
+      birthday: {
+        getDate: new Date(1990, 5, 5),
+      },
+      createdAt: {
+        getDate: new Date(1995, 5, 5),
+      },
+      updatedAt: {
+        getDate: new Date(2000, 5, 5),
+      },
+      biography: null,
+      profilePicture: null,
+      deletedAt: null,
+    } as unknown as User),
+  ],
+];
 
 describe('users', () => {
   describe('infrastructure', () => {
     describe('mappings', () => {
       describe('User to UserEntity', () => {
-        const id = 'id';
-        const email = 'john@DOE.com';
-        const username = 'John_Doe';
-        const hash = 'password hash';
-        const verificationStatus = true;
-        const blockedStatus = true;
-        const firstNameText = 'John';
-        const lastNameText = 'Doe';
-        const birthDate = new Date('2000-05-05');
-        const birthDateMilliseconds = birthDate.getTime();
-        const creationDate = new Date();
-        const creationDateMilliseconds = creationDate.getTime();
-        const bio = 'A nice person.';
+        test.each(validValues)(
+          'should map User to UserEntity keeping all the property values',
+          (user: User) => {
+            // Arrange
 
-        it('should map User to UserEntity keeping all the property values', () => {
-          // Arrange
-          const uniqueId = UniqueId.create(id);
-          const userEmail = Email.create(email);
-          const userName = UserName.create(username);
-          const passwordHash = PasswordHash.create(hash);
-          const isVerified = IsVerified.create(verificationStatus);
-          const isBlocked = IsBlocked.create(blockedStatus);
-          const firstName = FirstName.create(firstNameText);
-          const lastName = LastName.create(lastNameText);
-          const birthday = MillisecondsDate.createFromDate(birthDate);
-          const creationDate = MillisecondsDate.createFromMilliseconds(
-            creationDateMilliseconds,
-          );
-          const biography = Biography.create(bio);
+            // Act
+            const userEntity = userToUserEntity(user);
 
-          const user = User.create(
-            uniqueId,
-            userEmail,
-            userName,
-            passwordHash,
-            isVerified,
-            isBlocked,
-            firstName,
-            lastName,
-            birthday,
-            creationDate,
-            creationDate,
-            biography,
-          );
-
-          // Act
-          const userEntity = userToUserEntity(user);
-
-          // Assert
-          expect(userEntity.id).toBe(id);
-          expect(userEntity.email).toBe(email);
-          expect(userEntity.normalizedEmail).toBe(normalizeString(email));
-          expect(userEntity.userName).toBe(username);
-          expect(userEntity.normalizedUserName).toBe(normalizeString(username));
-          expect(userEntity.passwordHash).toBe(hash);
-          expect(userEntity.isVerified).toBe(verificationStatus);
-          expect(userEntity.isBlocked).toBe(blockedStatus);
-          expect(userEntity.firstName).toBe(firstNameText);
-          expect(userEntity.lastName).toBe(lastNameText);
-          expect(userEntity.birthday.getTime()).toBe(birthDateMilliseconds);
-          expect(userEntity.biography).toBe(bio);
-        });
+            // Assert
+            expect(userEntity.id).toBe(user.id.getId);
+            expect(userEntity.email).toBe(user.email.getEmail);
+            expect(userEntity.normalizedEmail).toBe(
+              user.email.getNormalizedEmail,
+            );
+            expect(userEntity.userName).toBe(user.userName.getUserName);
+            expect(userEntity.normalizedUserName).toBe(
+              user.userName.getNormalizedUserName,
+            );
+            expect(userEntity.passwordHash).toBe(
+              user.passwordHash.getPasswordHash,
+            );
+            expect(userEntity.isVerified).toBe(user.isVerified.getStatus);
+            expect(userEntity.isBlocked).toBe(user.isBlocked.getStatus);
+            expect(userEntity.firstName).toBe(user.firstName.getFirstName);
+            expect(userEntity.lastName).toBe(user.lastName.getLastName);
+            expect(userEntity.birthday).toBe(user.birthday.getDate);
+            expect(userEntity.createdAt).toBe(user.createdAt.getDate);
+            expect(userEntity.updatedAt).toBe(user.updatedAt.getDate);
+            if (user.biography)
+              expect(userEntity.biography).toBe(user.biography.getBiography);
+            else expect(userEntity.biography).toBeNull();
+            if (user.profilePicture)
+              expect(userEntity.profilePicture).toBe(
+                user.profilePicture.getUrl,
+              );
+            else expect(userEntity.profilePicture).toBeNull();
+            if (user.deletedAt)
+              expect(userEntity.deletedAt).toBe(user.deletedAt.getDate);
+            else expect(userEntity.deletedAt).toBeNull();
+          },
+        );
       });
     });
   });
