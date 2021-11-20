965e6c3f1df277a7be3c0131c39c48c48a9adc21
JM Orbegoso <dev@jmorbegoso.com>
2021-11-20 18:58:56 -0500

    Create update-user.handler.spec.ts

---
+++ src/users/application/commands/update-user/update-user.handler.spec.ts
@@ -0,0 +1,114 @@
+import { mocked } from 'ts-jest/utils';
+import { NotFoundException } from '@nestjs/common';
+import { UnitOfWork } from '../../../../core/domain/repositories';
+import { UserRepository } from '../../../../users/domain/repositories';
+import { User } from '../../../../users/domain/entities';
+import { UpdateUserHandler, UpdateUserCommand } from '..';
+
+describe('users', () => {
+  describe('application', () => {
+    describe('commands', () => {
+      describe('update-user', () => {
+        test('should throw NotFoundException', () => {
+          // Arrange
+          const unitOfWork = mocked<UnitOfWork>({
+            userRepository: {
+              getOne: jest.fn().mockReturnValue(null),
+            },
+          } as unknown as UnitOfWork);
+
+          const command = mocked<UpdateUserCommand>({
+            id: 'id-0',
+          } as unknown as UpdateUserCommand);
+
+          const handler = new UpdateUserHandler(unitOfWork);
+
+          // Act
+
+          // Assert
+          return expect(handler.execute(command)).rejects.toThrowError(
+            NotFoundException,
+          );
+        });
+
+        test('should call the method hashPassword of the EncryptionService, call the update method of the UserRepository and the commitChanges method of the UnitOfWork', async () => {
+          // Arrange
+          const user = mocked<User>({
+            id: {
+              getId: 'id-0',
+            },
+            email: {
+              getEmail: 'email0@email.com',
+            },
+            userName: {
+              getUserName: 'UserName0',
+            },
+            passwordHash: {
+              getPasswordHash: 'hash0',
+            },
+            isVerified: {
+              getStatus: true,
+            },
+            isBlocked: {
+              getStatus: false,
+            },
+            firstName: {
+              getFirstName: 'FirstName0',
+            },
+            lastName: {
+              getLastName: 'LastName0',
+            },
+            birthday: {
+              getMilliseconds: 1,
+            },
+            createdAt: {
+              getMilliseconds: 2,
+            },
+            updatedAt: {
+              getMilliseconds: 3,
+            },
+            biography: {
+              getBiography: 'A nice person 0.',
+            },
+            profilePicture: {
+              getUrl: 'https://www.example.com/0.jpg',
+            },
+            deletedAt: {
+              getMilliseconds: 4,
+            },
+            updateProfile: jest.fn(),
+          } as unknown as User);
+
+          const userRepository = mocked<UserRepository>({
+            getOne: jest.fn().mockReturnValue(user),
+            update: jest.fn(),
+          } as unknown as UserRepository);
+
+          const unitOfWork = mocked<UnitOfWork>({
+            userRepository: userRepository,
+            commitChanges: jest.fn(),
+          } as unknown as UnitOfWork);
+
+          const command = mocked<UpdateUserCommand>({
+            id: 'id-0',
+            firstName: 'FirstName0',
+            lastName: 'LastName0',
+            birthday: 1,
+            biography: 'A nice person 0.',
+            profilePicture: 'https://www.example.com/0.jpg',
+          } as unknown as UpdateUserCommand);
+
+          const handler = new UpdateUserHandler(unitOfWork);
+
+          // Act
+          await handler.execute(command);
+
+          // Assert
+          expect(user.updateProfile.mock.calls).toHaveLength(1);
+          expect(userRepository.update.mock.calls).toHaveLength(1);
+          expect(unitOfWork.commitChanges.mock.calls).toHaveLength(1);
+        });
+      });
+    });
+  });
+});
