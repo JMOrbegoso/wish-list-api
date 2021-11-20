ec76b4db626ae7a815c73fcd524736d2f12e4ea0
JM Orbegoso <dev@jmorbegoso.com>
2021-11-20 17:04:01 -0500

    Create get-users.handler.spec.ts

---
+++ src/users/application/queries/get-users/get-users.handler.spec.ts
@@ -0,0 +1,99 @@
+import { mocked } from 'ts-jest/utils';
+import { UnitOfWork } from '../../../../core/domain/repositories';
+import { User } from '../../../../users/domain/entities';
+import { GetUsersHandler, GetUsersQuery } from '..';
+
+describe('users', () => {
+  describe('application', () => {
+    describe('queries', () => {
+      describe('get-users', () => {
+        test('should return an empty users array', async () => {
+          // Arrange
+          const unitOfWork = mocked<UnitOfWork>({
+            userRepository: {
+              getAll: jest.fn().mockReturnValue([]),
+            },
+          } as unknown as UnitOfWork);
+
+          const query = mocked<GetUsersQuery>({
+            id: 1,
+          } as unknown as GetUsersQuery);
+
+          const handler = new GetUsersHandler(unitOfWork);
+
+          // Act
+          const outputUsers = await handler.execute(query);
+
+          // Assert
+          expect(outputUsers.length).toBe(0);
+        });
+
+        test('should return an array with one OutputUserDto', async () => {
+          // Arrange
+          const user = mocked<User>({
+            id: {
+              getId: 'id-0',
+            },
+            email: {
+              getEmail: 'email0@email.com',
+            },
+            userName: {
+              getUserName: 'UserName0',
+            },
+            passwordHash: {
+              getPasswordHash: 'hash0',
+            },
+            isVerified: {
+              getStatus: true,
+            },
+            isBlocked: {
+              getStatus: false,
+            },
+            firstName: {
+              getFirstName: 'FirstName0',
+            },
+            lastName: {
+              getLastName: 'LastName0',
+            },
+            birthday: {
+              getMilliseconds: 1,
+            },
+            createdAt: {
+              getMilliseconds: 2,
+            },
+            updatedAt: {
+              getMilliseconds: 3,
+            },
+            biography: {
+              getBiography: 'A nice person 0.',
+            },
+            profilePicture: {
+              getUrl: 'https://www.example.com/0.jpg',
+            },
+            deletedAt: {
+              getMilliseconds: 4,
+            },
+          } as unknown as User);
+
+          const unitOfWork = mocked<UnitOfWork>({
+            userRepository: {
+              getAll: jest.fn().mockReturnValue([user]),
+            },
+          } as unknown as UnitOfWork);
+
+          const query = mocked<GetUsersQuery>({
+            id: 1,
+          } as unknown as GetUsersQuery);
+
+          const handler = new GetUsersHandler(unitOfWork);
+
+          // Act
+          const outputUsers = await handler.execute(query);
+
+          // Assert
+          expect(outputUsers.length).toBe(1);
+        });
+      });
+    });
+  });
+});
