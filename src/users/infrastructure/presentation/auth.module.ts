e9ef67d3d27e5f034195094e7fec45922b20a7f8
JM Orbegoso <dev@jmorbegoso.com>
2021-11-22 09:30:08 -0500

    Update AuthModule adding LocalLoginPassportStrategy

--- src/users/infrastructure/presentation/auth.module.ts
+++ src/users/infrastructure/presentation/auth.module.ts
@@ -1,26 +1,33 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { CqrsModule } from '@nestjs/cqrs';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { UserRepositoryMongoDb } from '../persistence/repositories';
 import { UnitOfWork } from '../../../core/domain/repositories';
 import { UnitOfWorkMongoDb } from '../../../core/infrastructure/repositories';
+import { UserRepositoryMongoDb } from '../persistence/repositories';
 import { UserEntity } from '../persistence/entities';
+import { EncryptionService } from '../../application/services';
+import { EncryptionServiceBcrypt } from '../services';
 import { AuthController } from './auth.controller';
+import { LocalLoginHandler } from '../../../users/application/commands';
+import { LocalLoginPassportStrategy } from '../passport-strategies';
 
-const queryHandlers = [];
-const commandHandlers = [];
+const commandHandlers = [LocalLoginHandler];
+const passportStrategies = [LocalLoginPassportStrategy];
 
 @Module({
   controllers: [AuthController],
   imports: [
+    PassportModule,
     MikroOrmModule.forFeature([UserEntity]),
     UserRepositoryMongoDb,
     CqrsModule,
   ],
   providers: [
     { provide: UnitOfWork, useClass: UnitOfWorkMongoDb },
-    ...queryHandlers,
+    { provide: EncryptionService, useClass: EncryptionServiceBcrypt },
     ...commandHandlers,
+    ...passportStrategies,
   ],
 })
 export class AuthModule {}
