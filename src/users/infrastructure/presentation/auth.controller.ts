9f66d393b91e2e16fc3698f9ffb9d977545b1b9d
JM Orbegoso <dev@jmorbegoso.com>
2021-11-22 09:31:56 -0500

    Update AuthController adding the localLogin endpoint

--- src/users/infrastructure/presentation/auth.controller.ts
+++ src/users/infrastructure/presentation/auth.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Post, HttpCode, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { OutputUserDto } from '../../../users/application/dtos';
+import { LoginDto } from '../dtos';
 
 @ApiTags('AuthController')
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  @ApiBody({ type: LoginDto })
+  @ApiOkResponse({
+    description: 'User login successfully.',
+    type: OutputUserDto,
+  })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiUnauthorizedResponse({
+    description:
+      'User is deleted, blocked, not verified or the password is incorrect.',
+  })
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  @HttpCode(200)
+  localLogin(@Request() req): OutputUserDto {
+    return req.user;
+  }
+}
