9bfedb3d138657187e3eb3f0705349586116179a
JM Orbegoso <dev@jmorbegoso.com>
2021-11-24 12:36:49 -0500

    Update UpdateUserProfileHandler to throw exceptions on user deleted, blocked or not verified

--- src/users/application/commands/update-user-profile/update-user-profile.handler.spec.ts
+++ src/users/application/commands/update-user-profile/update-user-profile.handler.spec.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { mocked } from 'ts-jest/utils';
 import { UpdateUserProfileCommand, UpdateUserProfileHandler } from '..';
 import { UnitOfWork } from '../../../../core/domain/repositories';
@@ -33,6 +33,209 @@ describe('users', () => {
           // Assert
           return expect(handler.execute(command)).rejects.toThrowError(
             NotFoundException,
+          );
+        });
+
+        test('should throw BadRequestException because the user is deleted', () => {
+          // Arrange
+          const user = mocked<User>({
+            id: {
+              getId: 'id-0',
+            },
+            email: {
+              getEmail: 'email0@email.com',
+            },
+            username: {
+              getUsername: 'John_Doe_0',
+            },
+            passwordHash: {
+              getPasswordHash: 'hash0',
+            },
+            isVerified: true,
+            isBlocked: false,
+            firstName: {
+              getFirstName: 'FirstName0',
+            },
+            lastName: {
+              getLastName: 'LastName0',
+            },
+            birthday: {
+              getMilliseconds: 1,
+            },
+            createdAt: {
+              getMilliseconds: 2,
+            },
+            updatedAt: {
+              getMilliseconds: 3,
+            },
+            biography: {
+              getBiography: 'A nice person 0.',
+            },
+            profilePicture: {
+              getUrl: 'https://www.example.com/0.jpg',
+            },
+            deletedAt: {
+              getMilliseconds: 4,
+            },
+            isDeleted: true,
+          } as unknown as User);
+
+          const userRepository = mocked<UserRepository>({
+            getOne: jest.fn().mockReturnValue(user),
+          } as unknown as UserRepository);
+
+          const unitOfWork = mocked<UnitOfWork>({
+            userRepository: userRepository,
+          } as unknown as UnitOfWork);
+
+          const command = mocked<UpdateUserProfileCommand>({
+            id: 'id-0',
+          } as unknown as UpdateUserProfileCommand);
+
+          const handler = new UpdateUserProfileHandler(
+            unitOfWork,
+            userRepository,
+          );
+
+          // Act
+
+          // Assert
+          return expect(handler.execute(command)).rejects.toThrowError(
+            BadRequestException,
+          );
+        });
+
+        test('should throw BadRequestException because the user is blocked', () => {
+          // Arrange
+          const user = mocked<User>({
+            id: {
+              getId: 'id-0',
+            },
+            email: {
+              getEmail: 'email0@email.com',
+            },
+            username: {
+              getUsername: 'John_Doe_0',
+            },
+            passwordHash: {
+              getPasswordHash: 'hash0',
+            },
+            isVerified: true,
+            isBlocked: true,
+            firstName: {
+              getFirstName: 'FirstName0',
+            },
+            lastName: {
+              getLastName: 'LastName0',
+            },
+            birthday: {
+              getMilliseconds: 1,
+            },
+            createdAt: {
+              getMilliseconds: 2,
+            },
+            updatedAt: {
+              getMilliseconds: 3,
+            },
+            biography: {
+              getBiography: 'A nice person 0.',
+            },
+            profilePicture: {
+              getUrl: 'https://www.example.com/0.jpg',
+            },
+            deletedAt: null,
+            isDeleted: false,
+          } as unknown as User);
+
+          const userRepository = mocked<UserRepository>({
+            getOne: jest.fn().mockReturnValue(user),
+          } as unknown as UserRepository);
+
+          const unitOfWork = mocked<UnitOfWork>({
+            userRepository: userRepository,
+          } as unknown as UnitOfWork);
+
+          const command = mocked<UpdateUserProfileCommand>({
+            id: 'id-0',
+          } as unknown as UpdateUserProfileCommand);
+
+          const handler = new UpdateUserProfileHandler(
+            unitOfWork,
+            userRepository,
+          );
+
+          // Act
+
+          // Assert
+          return expect(handler.execute(command)).rejects.toThrowError(
+            BadRequestException,
+          );
+        });
+
+        test('should throw BadRequestException because the user is not verified', () => {
+          // Arrange
+          const user = mocked<User>({
+            id: {
+              getId: 'id-0',
+            },
+            email: {
+              getEmail: 'email0@email.com',
+            },
+            username: {
+              getUsername: 'John_Doe_0',
+            },
+            passwordHash: {
+              getPasswordHash: 'hash0',
+            },
+            isVerified: false,
+            isBlocked: false,
+            firstName: {
+              getFirstName: 'FirstName0',
+            },
+            lastName: {
+              getLastName: 'LastName0',
+            },
+            birthday: {
+              getMilliseconds: 1,
+            },
+            createdAt: {
+              getMilliseconds: 2,
+            },
+            updatedAt: {
+              getMilliseconds: 3,
+            },
+            biography: {
+              getBiography: 'A nice person 0.',
+            },
+            profilePicture: {
+              getUrl: 'https://www.example.com/0.jpg',
+            },
+            deletedAt: null,
+            isDeleted: false,
+          } as unknown as User);
+
+          const userRepository = mocked<UserRepository>({
+            getOne: jest.fn().mockReturnValue(user),
+          } as unknown as UserRepository);
+
+          const unitOfWork = mocked<UnitOfWork>({
+            userRepository: userRepository,
+          } as unknown as UnitOfWork);
+
+          const command = mocked<UpdateUserProfileCommand>({
+            id: 'id-0',
+          } as unknown as UpdateUserProfileCommand);
+
+          const handler = new UpdateUserProfileHandler(
+            unitOfWork,
+            userRepository,
+          );
+
+          // Act
+
+          // Assert
+          return expect(handler.execute(command)).rejects.toThrowError(
+            BadRequestException,
           );
         });
 
