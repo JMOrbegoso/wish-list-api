948e308d6e34d567786528790278fc9e59d7d871
JM Orbegoso <dev@jmorbegoso.com>
2021-11-28 20:39:46 -0500

    Create mapper of RefreshTokenEntity to RefreshToken

---
+++ src/users/infrastructure/mappings/refresh-token-entity-to-refresh-token.mapper.ts
@@ -0,0 +1,36 @@
+import { MillisecondsDate, UniqueId } from '../../../core/domain/value-objects';
+import { RefreshToken } from '../../domain/entities';
+import { Ip, SecondsDuration } from '../../domain/value-objects';
+import { RefreshTokenEntity } from '../persistence/entities';
+
+export function refreshTokenEntityToRefreshToken(
+  refreshTokenEntity: RefreshTokenEntity,
+): RefreshToken {
+  const id = UniqueId.create(refreshTokenEntity.id);
+  const userId = UniqueId.create(refreshTokenEntity.userId);
+  const createdAt = MillisecondsDate.createFromDate(
+    refreshTokenEntity.createdAt,
+  );
+  const secondsDuration = SecondsDuration.create(refreshTokenEntity.duration);
+  const ip = Ip.create(refreshTokenEntity.ip);
+  const replacedAt = refreshTokenEntity.replacedAt
+    ? MillisecondsDate.createFromDate(refreshTokenEntity.replacedAt)
+    : null;
+  const replacedBy = refreshTokenEntity.replacedBy
+    ? UniqueId.create(refreshTokenEntity.replacedBy)
+    : null;
+  const revokedAt = refreshTokenEntity.revokedAt
+    ? MillisecondsDate.createFromDate(refreshTokenEntity.revokedAt)
+    : null;
+
+  return RefreshToken.create(
+    id,
+    userId,
+    createdAt,
+    secondsDuration,
+    ip,
+    replacedAt,
+    replacedBy,
+    revokedAt,
+  );
+}
