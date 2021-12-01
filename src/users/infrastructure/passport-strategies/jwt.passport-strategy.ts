19e3e02843a488a0a33aeed5fc65b3531f231502
JM Orbegoso <dev@jmorbegoso.com>
2021-12-01 07:27:53 -0500

    Update JwtPassportStrategy to return an OutputUserDto

--- src/users/infrastructure/passport-strategies/jwt.passport-strategy.ts
+++ src/users/infrastructure/passport-strategies/jwt.passport-strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { OutputUserDto } from '../../application/dtos';
 
 @Injectable()
 export class JwtPassportStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +13,23 @@ export class JwtPassportStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload): Promise<{ id: string }> {
-    return { id: payload.sub };
+  async validate(payload): Promise<OutputUserDto> {
+    const outputUserDto = new OutputUserDto();
+    outputUserDto.id = payload.sub;
+    outputUserDto.email = payload.email;
+    outputUserDto.username = payload.username;
+    outputUserDto.isVerified = payload.isVerified;
+    outputUserDto.isBlocked = payload.isBlocked;
+    outputUserDto.firstName = payload.firstName;
+    outputUserDto.lastName = payload.lastName;
+    outputUserDto.birthday = payload.birthday;
+    outputUserDto.createdAt = payload.createdAt;
+    outputUserDto.updatedAt = payload.updatedAt;
+    outputUserDto.biography = payload.biography;
+    outputUserDto.roles = payload.roles;
+    outputUserDto.profilePicture = payload.profilePicture;
+    outputUserDto.deletedAt = payload.deletedAt;
+
+    return { ...outputUserDto };
   }
 }
