c441d6ba6bb36fb41eb347811567850e2c9c3145
JM Orbegoso <dev@jmorbegoso.com>
2021-12-06 14:15:37 -0500

    Create CompleteWish command

---
+++ src/wishes/application/commands/complete-wish/complete-wish.command.ts
@@ -0,0 +1,6 @@
+export class CompleteWishCommand {
+  constructor(
+    public readonly id: string,
+    public readonly millisecondsDate: number,
+  ) {}
+}
--- src/wishes/application/commands/index.ts
+++ src/wishes/application/commands/index.ts
@@ -1,2 +1,5 @@
 export * from './create-wish/create-wish.command';
 export * from './create-wish/create-wish.handler';
+
+export * from './complete-wish/complete-wish.command';
+export * from './complete-wish/complete-wish.handler';
