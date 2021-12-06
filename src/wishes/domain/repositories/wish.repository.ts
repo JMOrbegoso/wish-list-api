9d2ffbffd16397505a37220d009f8886d4070b38
JM Orbegoso <dev@jmorbegoso.com>
2021-12-06 17:45:18 -0500

    Update WishRepository adding the method getWishStageById

--- src/wishes/domain/repositories/wish.repository.ts
+++ src/wishes/domain/repositories/wish.repository.ts
@@ -1,8 +1,10 @@
 import { Repository } from '../../../core/domain/repositories';
 import { UniqueId } from '../../../core/domain/value-objects';
-import { Wish } from '../entities';
+import { Wish, WishStage } from '../entities';
 
 export interface WishRepository extends Repository<Wish> {
+  getWishStageById(id: UniqueId): Promise<WishStage>;
+
   getAllPublicWishes(): Promise<Wish[]>;
 
   getAllWishesByWisher(wisherId: UniqueId): Promise<Wish[]>;
