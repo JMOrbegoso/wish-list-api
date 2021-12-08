2034d482ca4b05df20ee6f24b9fcc1499360f6e9
JM Orbegoso <dev@jmorbegoso.com>
2021-12-07 19:55:26 -0500

    Create mapper WishStageEntity to WishStage

--- src/wishes/infrastructure/mappings/index.ts
+++ src/wishes/infrastructure/mappings/index.ts
@@ -2,3 +2,4 @@ export * from './wisher-to-wisher-entity.mapper';
 export * from './wisher-entity-to-wisher.mapper';
 
 export * from './wish-stage-to-wish-stage-entity.mapper';
+export * from './wish-stage-entity-to-wish-stage.mapper';
---
+++ src/wishes/infrastructure/mappings/wish-stage-entity-to-wish-stage.mapper.ts
@@ -0,0 +1,21 @@
+import {
+  MillisecondsDate,
+  UniqueId,
+  WebUrl,
+} from '../../../core/domain/value-objects';
+import { WishStage } from '../../domain/entities';
+import { WishDescription, WishTitle } from '../../domain/value-objects';
+import { WishStageEntity } from '../persistence/entities';
+
+export function wishStageEntityToWishStage(
+  wishStageEntity: WishStageEntity,
+): WishStage {
+  const id = UniqueId.create(wishStageEntity.id);
+  const title = WishTitle.create(wishStageEntity.title);
+  const description = WishDescription.create(wishStageEntity.description);
+  const createdAt = MillisecondsDate.createFromDate(wishStageEntity.createdAt);
+  const urls = wishStageEntity.urls.map((url) => WebUrl.create(url));
+  const imageUrls = wishStageEntity.imageUrls.map((url) => WebUrl.create(url));
+
+  return WishStage.create(id, title, description, createdAt, urls, imageUrls);
+}
