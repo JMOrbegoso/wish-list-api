f55899c077e53b725359d1ee9e580f90c50acbb7
JM Orbegoso <dev@jmorbegoso.com>
2021-12-09 16:43:49 -0500

    Create CompleteWishDto

--- src/wishes/infrastructure/presentation/dto/index.ts
+++ src/wishes/infrastructure/presentation/dto/index.ts
@@ -2,3 +2,4 @@ export * from './create-wish.dto';
 export * from './wish-id.dto';
 export * from './wisher-id.dto';
 export * from './change-wish-privacy-level.dto';
+export * from './complete-wish.dto';
