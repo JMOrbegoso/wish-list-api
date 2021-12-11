2fc599396dc6d69969a794b3d68ba3c74443de35
JM Orbegoso <dev@jmorbegoso.com>
2021-12-11 09:41:44 -0500

    Create RolesGuard

---
+++ src/users/infrastructure/presentation/guards/roles.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Role } from '../../../domain/value-objects';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly validRoles: Role[]) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    const validRoles = this.validRoles.map((role) => role.getRole);
+    return validRoles.some((role) => user.roles.includes(role));
+  }
+}
