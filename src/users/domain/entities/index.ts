bc35adc7120a84775f0719ffccc703f2e3dc22e5
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2021-12-22 13:56:56 -0500

    Create InvalidRefreshTokenError

--- src/users/domain/entities/index.ts
+++ src/users/domain/entities/index.ts
@@ -3,5 +3,7 @@ export * from './user/deleted-user-cannot-be-updated.error';
 export * from './user/blocked-user-cannot-be-updated.error';
 export * from './user/unverified-user-cannot-be-updated.error';
 
-export * from './verification-code/verification-code';
 export * from './refresh-token/refresh-token';
+export * from './refresh-token/invalid-refresh-token.error';
+
+export * from './verification-code/verification-code';
