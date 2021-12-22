53bf54cfaf4c66c84bf01c73abdca356c9701632
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2021-12-22 14:00:02 -0500

    Create DuplicatedRefreshTokenError

--- src/users/domain/entities/index.ts
+++ src/users/domain/entities/index.ts
@@ -5,5 +5,6 @@ export * from './user/unverified-user-cannot-be-updated.error';
 
 export * from './refresh-token/refresh-token';
 export * from './refresh-token/invalid-refresh-token.error';
+export * from './refresh-token/duplicated-refresh-token.error';
 
 export * from './verification-code/verification-code';
