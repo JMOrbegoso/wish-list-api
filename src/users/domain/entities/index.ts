de1c5155957929fabe41b7d90bb4f39df691c7ec
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2021-12-22 16:21:31 -0500

    Create RefreshTokenNotFoundError

--- src/users/domain/entities/index.ts
+++ src/users/domain/entities/index.ts
@@ -6,5 +6,6 @@ export * from './user/unverified-user-cannot-be-updated.error';
 export * from './refresh-token/refresh-token';
 export * from './refresh-token/invalid-refresh-token.error';
 export * from './refresh-token/duplicated-refresh-token.error';
+export * from './refresh-token/refresh-token-not-found.error';
 
 export * from './verification-code/verification-code';
