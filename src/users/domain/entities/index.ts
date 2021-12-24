bbcc086a60e801876526b27533c89e0071ea1720
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2021-12-23 19:20:26 -0500

    Create InvalidRefreshTokensError

--- src/users/domain/entities/index.ts
+++ src/users/domain/entities/index.ts
@@ -5,6 +5,7 @@ export * from './user/unverified-user-cannot-be-updated.error';
 
 export * from './refresh-token/refresh-token';
 export * from './refresh-token/invalid-refresh-token.error';
+export * from './refresh-token/invalid-refresh-tokens.error';
 export * from './refresh-token/duplicated-refresh-token.error';
 export * from './refresh-token/refresh-token-not-found.error';
 
