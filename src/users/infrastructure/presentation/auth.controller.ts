9dcb3780b9fb51b3618553b432260df7ba0a1825
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2021-12-24 09:51:18 -0500

    Update controllers removing Swagger documentation

--- src/users/infrastructure/presentation/auth.controller.ts
+++ src/users/infrastructure/presentation/auth.controller.ts
@@ -9,14 +9,6 @@ import {
 } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import {
-  ApiBadRequestResponse,
-  ApiBody,
-  ApiNotFoundResponse,
-  ApiOkResponse,
-  ApiTags,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
-import {
   LocalLoginCommand,
   RefreshAccessTokenCommand,
   VerifyUserCommand,
@@ -24,21 +16,10 @@ import {
 import { AuthTokensDto } from '../../application/dtos';
 import { LoginDto, RefreshTokenDto } from '../dtos';
 
-@ApiTags('AuthController')
 @Controller()
 export class AuthController {
   constructor(private commandBus: CommandBus) {}
 
-  @ApiBody({ type: LoginDto })
-  @ApiOkResponse({
-    description: 'User login successfully.',
-    type: AuthTokensDto,
-  })
-  @ApiNotFoundResponse({ description: 'User not found.' })
-  @ApiUnauthorizedResponse({
-    description:
-      'User is deleted, blocked, not verified or the password is incorrect.',
-  })
   @Post('login')
   @HttpCode(200)
   async login(
@@ -53,12 +34,6 @@ export class AuthController {
     return await this.commandBus.execute(command);
   }
 
-  @ApiBody({ required: true, type: RefreshTokenDto })
-  @ApiOkResponse({
-    description: 'Auth tokens successfully refreshed.',
-    type: AuthTokensDto,
-  })
-  @ApiUnauthorizedResponse({ description: 'Refresh token is invalid.' })
   @Post('refresh')
   @HttpCode(200)
   async refresh(
@@ -69,9 +44,6 @@ export class AuthController {
     return await this.commandBus.execute(command);
   }
 
-  @ApiOkResponse({ description: 'User verified successfully.' })
-  @ApiNotFoundResponse({ description: 'User not found.' })
-  @ApiBadRequestResponse({ description: 'Something went wrong.' })
   @Get('verify')
   async verify(@Query('code') code: string): Promise<void> {
     const command = new VerifyUserCommand(code);
