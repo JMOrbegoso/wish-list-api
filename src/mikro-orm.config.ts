650545600eb563cddd2dae81d365e3585455f674
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2021-12-29 10:03:28 -0500

    Update mikro-orm.config.ts fixing the clientUrl

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,23 +1,20 @@
 import { Options as MikroOrmOptions } from '@mikro-orm/core';
 import { MongoHighlighter } from '@mikro-orm/mongo-highlighter';
+import { MongoDriver } from '@mikro-orm/mongodb';
 import { Logger } from '@nestjs/common';
 import config from './config';
 
 const logger = new Logger('MikroORM');
 
-const mikroOrmConfig: MikroOrmOptions = {
+const mikroOrmConfig: MikroOrmOptions<MongoDriver> = {
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
   debug: true,
   highlighter: new MongoHighlighter(),
   logger: logger.log.bind(logger),
   type: 'mongo',
-  host: config.DB_HOST,
-  user: config.DB_USER,
-  password: config.DB_PASSWORD,
   dbName: config.DB_DATABASE,
-  port: config.DB_PORT,
+  clientUrl: `mongodb://${config.DB_USER}:${config.DB_PASSWORD}@${config.DB_HOST}:${config.DB_PORT}`,
   ensureIndexes: true,
 };
-
 export default mikroOrmConfig;
