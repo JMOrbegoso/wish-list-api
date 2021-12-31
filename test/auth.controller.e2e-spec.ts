ca7dbb7e9ff4e2135f04d9513ec2f756963175c6
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2021-12-30 20:04:18 -0500

    Update auth.controller.e2e-spec.ts adding tests for /refresh

--- test/auth.controller.e2e-spec.ts
+++ test/auth.controller.e2e-spec.ts
@@ -6,12 +6,18 @@ import { AppModule } from '../src/app.module';
 import mikroOrmConfig from '../src/mikro-orm.config';
 import { AuthTokensDto } from '../src/users/application/dtos';
 import { Password, Role, Username } from '../src/users/domain/value-objects';
-import { LoginDto } from '../src/users/infrastructure/dtos';
+import { LoginDto, RefreshTokenDto } from '../src/users/infrastructure/dtos';
 import {
   RefreshTokenEntity,
   UserEntity,
 } from '../src/users/infrastructure/persistence/entities';
-import { UserDb, dropDatabase, seedUser } from './helpers';
+import {
+  RefreshTokenDb,
+  UserDb,
+  dropDatabase,
+  seedRefreshToken,
+  seedUser,
+} from './helpers';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
@@ -22,6 +28,12 @@ describe('AuthController (e2e)', () => {
   let user_2: UserDb;
   let user_3: UserDb;
   let user_4: UserDb;
+  let refreshToken_1: RefreshTokenDb;
+  let refreshToken_2: RefreshTokenDb;
+  let refreshToken_3: RefreshTokenDb;
+  let refreshToken_4: RefreshTokenDb;
+  let refreshToken_5: RefreshTokenDb;
+  let refreshToken_6: RefreshTokenDb;
 
   async function seedDatabase(): Promise<void> {
     const database = mongoClient.db(mikroOrmConfig.dbName);
@@ -96,6 +108,51 @@ describe('AuthController (e2e)', () => {
       'https://www.example.com/anne/',
       null,
       [Role.basic().getRole],
+    );
+
+    refreshToken_1 = await seedRefreshToken(
+      database,
+      user_4._id.toString(),
+      new Date(2000, 10, 10),
+    );
+
+    refreshToken_2 = await seedRefreshToken(
+      database,
+      user_4._id.toString(),
+      new Date(),
+      1000,
+      '192.168.0.1',
+    );
+
+    refreshToken_3 = await seedRefreshToken(
+      database,
+      user_4._id.toString(),
+      undefined,
+      undefined,
+      undefined,
+      new Date(),
+      refreshToken_1._id.toString(),
+    );
+
+    refreshToken_4 = await seedRefreshToken(
+      database,
+      user_4._id.toString(),
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      new Date(),
+    );
+
+    refreshToken_5 = await seedRefreshToken(database, user_1._id.toString());
+
+    refreshToken_6 = await seedRefreshToken(
+      database,
+      user_2._id.toString(),
+      undefined,
+      undefined,
+      '192.168.0.1',
     );
   }
 
@@ -413,6 +470,240 @@ describe('AuthController (e2e)', () => {
     });
   });
 
+  describe('/refresh', () => {
+    describe('POST', () => {
+      describe(`should return 400`, () => {
+        it(`RefreshToken should not be empty`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({} as RefreshTokenDto)
+            .expect(400)
+            .expect(({ body }) =>
+              expect(
+                (body.message as string[]).some((m) =>
+                  m.match(/refresh_token should not be empty/i),
+                ),
+              ).toBeTruthy(),
+            );
+        });
+
+        it(`RefreshToken should not be empty`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({ refresh_token: '' } as RefreshTokenDto)
+            .expect(400)
+            .expect(({ body }) =>
+              expect(
+                (body.message as string[]).some((m) =>
+                  m.match(/refresh_token should not be empty/i),
+                ),
+              ).toBeTruthy(),
+            );
+        });
+
+        it(`RefreshToken must be a MongoDB ID`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({
+              refresh_token: 'refresh_token',
+            } as unknown as RefreshTokenDto)
+            .expect(400)
+            .expect(({ body }) =>
+              expect(
+                (body.message as string[]).some((m) =>
+                  m.match(/refresh_token must be a mongodb id/i),
+                ),
+              ).toBeTruthy(),
+            );
+        });
+      });
+
+      describe(`should return 401`, () => {
+        it(`Refresh Token not found`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({
+              refresh_token: new ObjectId().toString(),
+            } as RefreshTokenDto)
+            .expect(401);
+        });
+
+        it(`Refresh Token has expired`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({
+              refresh_token: refreshToken_1._id.toString(),
+            } as RefreshTokenDto)
+            .expect(401)
+            .expect(async () => {
+              const database = mongoClient.db(mikroOrmConfig.dbName);
+
+              const refreshToken: RefreshTokenEntity = await database
+                .collection('refresh-tokens')
+                .findOne({ _id: new ObjectId(refreshToken_1._id) });
+
+              expect(refreshToken).toBeTruthy();
+              expect(refreshToken.replacedBy).toBeFalsy();
+              expect(refreshToken.replacedAt).toBeFalsy();
+              expect(refreshToken.revokedAt).toBeFalsy();
+            });
+        });
+
+        it(`Refresh Token has was used, should revoke all the refresh tokens of the User and Ip address`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({
+              refresh_token: refreshToken_3._id.toString(),
+            } as RefreshTokenDto)
+            .expect(401)
+            .expect(async () => {
+              const database = mongoClient.db(mikroOrmConfig.dbName);
+
+              const refreshTokensDb: RefreshTokenEntity[] = await database
+                .collection('refresh-tokens')
+                .find()
+                .toArray();
+
+              // Check if the refreshToken_2 was automatically revoked
+              // (Refresh Token generated by the same User)
+              const validRefreshToken1 = refreshTokensDb.find(
+                (rt) => rt._id.toString() === refreshToken_2._id.toString(),
+              );
+              expect(validRefreshToken1).toBeTruthy();
+              expect(validRefreshToken1.replacedBy).toBeFalsy();
+              expect(validRefreshToken1.replacedAt).toBeFalsy();
+              expect(validRefreshToken1.revokedAt).toBeTruthy();
+
+              // Check if the refreshToken_5 was automatically revoked
+              // (Refresh Token generated by different User but the same Ip Address)
+              const validRefreshToken2 = refreshTokensDb.find(
+                (rt) => rt._id.toString() === refreshToken_5._id.toString(),
+              );
+              expect(validRefreshToken2).toBeTruthy();
+              expect(validRefreshToken2.replacedBy).toBeFalsy();
+              expect(validRefreshToken2.replacedAt).toBeFalsy();
+              expect(validRefreshToken2.revokedAt).toBeTruthy();
+
+              // Check if the refreshToken_6 was NOT automatically revoked
+              // (Refresh Token generated by different User and different Ip Address)
+              const validRefreshToken3 = refreshTokensDb.find(
+                (rt) => rt._id.toString() === refreshToken_6._id.toString(),
+              );
+              expect(validRefreshToken3).toBeTruthy();
+              expect(validRefreshToken3.replacedBy).toBeFalsy();
+              expect(validRefreshToken3.replacedAt).toBeFalsy();
+              expect(validRefreshToken3.revokedAt).toBeFalsy();
+            });
+        });
+
+        it(`Refresh Token has was revoked, should revoke all the refresh tokens of the User and Ip address`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({
+              refresh_token: refreshToken_4._id.toString(),
+            } as RefreshTokenDto)
+            .expect(401)
+            .expect(async () => {
+              const database = mongoClient.db(mikroOrmConfig.dbName);
+
+              const refreshTokensDb: RefreshTokenEntity[] = await database
+                .collection('refresh-tokens')
+                .find()
+                .toArray();
+
+              // Check if the refreshToken_2 was automatically revoked
+              // (Refresh Token generated by the same User)
+              const validRefreshToken1 = refreshTokensDb.find(
+                (rt) => rt._id.toString() === refreshToken_2._id.toString(),
+              );
+              expect(validRefreshToken1).toBeTruthy();
+              expect(validRefreshToken1.replacedBy).toBeFalsy();
+              expect(validRefreshToken1.replacedAt).toBeFalsy();
+              expect(validRefreshToken1.revokedAt).toBeTruthy();
+
+              // Check if the refreshToken_5 was automatically revoked
+              // (Refresh Token generated by different User but the same Ip Address)
+              const validRefreshToken2 = refreshTokensDb.find(
+                (rt) => rt._id.toString() === refreshToken_5._id.toString(),
+              );
+              expect(validRefreshToken2).toBeTruthy();
+              expect(validRefreshToken2.replacedBy).toBeFalsy();
+              expect(validRefreshToken2.replacedAt).toBeFalsy();
+              expect(validRefreshToken2.revokedAt).toBeTruthy();
+
+              // Check if the refreshToken_6 was NOT automatically revoked
+              // (Refresh Token generated by different User and different Ip Address)
+              const validRefreshToken3 = refreshTokensDb.find(
+                (rt) => rt._id.toString() === refreshToken_6._id.toString(),
+              );
+              expect(validRefreshToken3).toBeTruthy();
+              expect(validRefreshToken3.replacedBy).toBeFalsy();
+              expect(validRefreshToken3.replacedAt).toBeFalsy();
+              expect(validRefreshToken3.revokedAt).toBeFalsy();
+            });
+        });
+      });
+
+      describe(`should return 200`, () => {
+        it(`Auth tokens successfully refreshed`, () => {
+          return request(app.getHttpServer())
+            .post('/refresh')
+            .send({
+              refresh_token: refreshToken_2._id.toString(),
+            } as RefreshTokenDto)
+            .expect(200)
+            .expect(async ({ body }) => {
+              const authTokens = body as AuthTokensDto;
+
+              expect(authTokens).toBeTruthy();
+              expect(authTokens.access_token).toBeTruthy();
+              expect(authTokens.refresh_token).toBeTruthy();
+
+              const database = mongoClient.db(mikroOrmConfig.dbName);
+              const refreshTokensDb: RefreshTokenEntity[] = await database
+                .collection('refresh-tokens')
+                .find()
+                .toArray();
+
+              // Check if the new refresh token was successfully created
+              const newRefreshToken = refreshTokensDb.find(
+                (rt) => rt._id.toString() === authTokens.refresh_token,
+              );
+
+              expect(newRefreshToken).toBeTruthy();
+              expect(newRefreshToken.user.toString()).toBe(
+                user_4._id.toString(),
+              );
+              expect(newRefreshToken.createdAt).toBeTruthy();
+              expect(newRefreshToken.duration).toBeTruthy();
+              expect(newRefreshToken.ipAddress).toBeTruthy();
+              expect(newRefreshToken.replacedAt).toBeNull();
+              expect(newRefreshToken.replacedBy).toBeNull();
+              expect(newRefreshToken.revokedAt).toBeNull();
+
+              // Check if the refreshToken_2 was used
+              const usedRefreshToken = refreshTokensDb.find(
+                (rt) => rt._id.toString() === refreshToken_2._id.toString(),
+              );
+
+              expect(usedRefreshToken).toBeTruthy();
+              expect(usedRefreshToken.user.toString()).toBe(
+                user_4._id.toString(),
+              );
+              expect(usedRefreshToken.createdAt).toBeTruthy();
+              expect(usedRefreshToken.duration).toBeTruthy();
+              expect(usedRefreshToken.ipAddress).toBeTruthy();
+              expect(usedRefreshToken.replacedAt).not.toBeNull();
+              expect(usedRefreshToken.replacedBy).toBe(
+                newRefreshToken._id.toString(),
+              );
+              expect(usedRefreshToken.revokedAt).toBeNull();
+            });
+        });
+      });
+    });
+  });
+
   describe('/verify', () => {
     describe('GET', () => {
       describe(`should return 400`, () => {
@@ -459,6 +750,7 @@ describe('AuthController (e2e)', () => {
             .expect(200)
             .expect(async () => {
               const database = mongoClient.db(mikroOrmConfig.dbName);
+
               const user: UserEntity = await database
                 .collection('users')
                 .findOne({ _id: new ObjectId(user_3._id) });
