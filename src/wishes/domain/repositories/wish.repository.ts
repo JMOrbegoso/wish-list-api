821bb73680d121de324cdf7c544861e4bca6d9bc
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-04 07:54:55 -0500

    Update WishRepository adding the methods 'addWishStage', 'updateWishStage', 'deleteWishStage'

--- src/wishes/domain/repositories/wish.repository.ts
+++ src/wishes/domain/repositories/wish.repository.ts
@@ -3,8 +3,6 @@ import { UniqueId } from '../../../shared/domain/value-objects';
 import { Wish, WishStage, Wisher } from '../entities';
 
 export abstract class WishRepository implements Repository<Wish> {
-  abstract getWishStageById(wishStageId: UniqueId): Promise<WishStage>;
-
   abstract getAllPublicWishes(): Promise<Wish[]>;
 
   abstract getAllWishesByWisher(wisherId: UniqueId): Promise<Wish[]>;
@@ -21,4 +19,9 @@ export abstract class WishRepository implements Repository<Wish> {
   abstract getWisherById(id: UniqueId): Promise<Wisher>;
   abstract addWisher(wisher: Wisher): void;
   abstract updateWisher(wisher: Wisher): void;
+
+  abstract getWishStageById(id: UniqueId): Promise<WishStage>;
+  abstract addWishStage(wishStage: WishStage): void;
+  abstract updateWishStage(wishStage: WishStage): void;
+  abstract deleteWishStage(id: UniqueId): void;
 }
