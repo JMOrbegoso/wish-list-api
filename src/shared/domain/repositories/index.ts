8ddf786cefe5e122001c719915c32036779985c5
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-11 21:18:42 -0500

    Delete generic Repository

--- src/shared/domain/repositories/index.ts
+++ src/shared/domain/repositories/index.ts
@@ -1,2 +1,1 @@
-export * from './repository';
 export * from './unit-of-work';
--- src/shared/domain/repositories/repository.ts
+++
@@ -1,8 +0,0 @@
-import { AggregateRoot } from '../entities';
-import { UniqueId } from '../value-objects';
-
-export interface Repository<T extends AggregateRoot> {
-  getAll(): Promise<T[]>;
-
-  getOneById(id: UniqueId): Promise<T>;
-}
