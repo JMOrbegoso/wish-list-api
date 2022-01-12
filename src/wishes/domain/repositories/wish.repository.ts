8ceb92e6cab502d15c1bbd28430881b0b0164992
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-11 21:19:17 -0500

    Update WishRepository using specialized entity identifications

--- src/wishes/domain/repositories/wish.repository.ts
+++ src/wishes/domain/repositories/wish.repository.ts
@@ -1,27 +1,32 @@
-import { Repository } from '../../../shared/domain/repositories';
-import { UniqueId } from '../../../shared/domain/value-objects';
-import { Wish, WishStage, Wisher } from '../entities';
+import {
+  Wish,
+  WishId,
+  WishStage,
+  WishStageId,
+  Wisher,
+  WisherId,
+} from '../entities';
 
-export abstract class WishRepository implements Repository<Wish> {
+export abstract class WishRepository {
   abstract getAllPublicWishes(): Promise<Wish[]>;
 
-  abstract getAllWishesByWisher(wisherId: UniqueId): Promise<Wish[]>;
+  abstract getAllWishesByWisher(wisherId: WisherId): Promise<Wish[]>;
 
-  abstract getWishByWishStageId(id: UniqueId): Promise<Wish>;
+  abstract getWishByWishStageId(id: WishStageId): Promise<Wish>;
 
   abstract getAll(): Promise<Wish[]>;
 
-  abstract getOneById(id: UniqueId): Promise<Wish>;
+  abstract getOneById(id: WishId): Promise<Wish>;
 
   abstract addWish(wish: Wish): void;
   abstract updateWish(wish: Wish): void;
 
-  abstract getWisherById(id: UniqueId): Promise<Wisher>;
+  abstract getWisherById(id: WisherId): Promise<Wisher>;
   abstract addWisher(wisher: Wisher): void;
   abstract updateWisher(wisher: Wisher): void;
 
-  abstract getWishStageById(id: UniqueId): Promise<WishStage>;
-  abstract addWishStage(wishStage: WishStage, wishId: UniqueId): void;
+  abstract getWishStageById(id: WishStageId): Promise<WishStage>;
+  abstract addWishStage(wishStage: WishStage, wishId: WishId): void;
   abstract updateWishStage(wishStage: WishStage): void;
-  abstract deleteWishStage(id: UniqueId): void;
+  abstract deleteWishStage(id: WishStageId): void;
 }
