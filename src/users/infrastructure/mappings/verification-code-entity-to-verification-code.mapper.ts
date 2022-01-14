7d5ac40461b53d3237ebf6f028a738b19d968bc5
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-14 12:01:49 -0500

    Create mapper VerificationCodeEntity to VerificationCode

---
+++ src/users/infrastructure/mappings/verification-code-entity-to-verification-code.mapper.ts
@@ -0,0 +1,18 @@
+import { MillisecondsDate } from '../../../shared/domain/value-objects';
+import { VerificationCode, VerificationCodeId } from '../../domain/entities';
+import { SecondsDuration } from '../../domain/value-objects';
+import { VerificationCodeEntity } from '../persistence/entities';
+
+export function verificationCodeEntityToVerificationCode(
+  verificationCodeEntity: VerificationCodeEntity,
+): VerificationCode {
+  const verificationCodeId = VerificationCodeId.create(
+    verificationCodeEntity.id,
+  );
+  const createdAt = MillisecondsDate.createFromDate(
+    verificationCodeEntity.createdAt,
+  );
+  const duration = SecondsDuration.create(verificationCodeEntity.duration);
+
+  return VerificationCode.create(verificationCodeId, createdAt, duration);
+}
