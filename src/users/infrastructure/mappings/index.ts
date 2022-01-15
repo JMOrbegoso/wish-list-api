84c714797b392be80c4ded4a2187c485e5f3e670
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-14 19:09:30 -0500

    Delete useless User mappings

--- src/users/infrastructure/mappings/index.ts
+++ src/users/infrastructure/mappings/index.ts
@@ -1,8 +1,5 @@
-export * from './user-to-user-entity.mapper';
 export * from './user-entity-to-user.mapper';
 
-export * from './verification-code-to-verification-code-entity.mapper';
 export * from './verification-code-entity-to-verification-code.mapper';
 
-export * from './refresh-token-to-refresh-token-entity.mapper';
 export * from './refresh-token-entity-to-refresh-token.mapper';
