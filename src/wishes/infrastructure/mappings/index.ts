70bfbee51df5cbd3a88ebf5f105d3deb289d64d2
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-14 19:16:49 -0500

    Delete useless Wish mappings

--- src/wishes/infrastructure/mappings/index.ts
+++ src/wishes/infrastructure/mappings/index.ts
@@ -1,8 +1,5 @@
-export * from './wish-to-wish-entity.mapper';
 export * from './wish-entity-to-wish.mapper';
 
-export * from './wisher-to-wisher-entity.mapper';
 export * from './wisher-entity-to-wisher.mapper';
 
-export * from './wish-stage-to-wish-stage-entity.mapper';
 export * from './wish-stage-entity-to-wish-stage.mapper';
